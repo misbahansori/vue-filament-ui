5a2296f25fcca341ac8b6c4d486c5fe366b20bed
Misbah Ansori <misbah.ansori24@gmail.com>
2023-04-08 07:25:21 +0700

    wip

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,27 @@ import Button from "./Components/Button.vue";
 import TextInput from "./Components/TextInput.vue";
 </script>
 <template>
-  <div class="flex min-h-screen items-center p-12">
-    <Button>This is a button</Button>
-    <TextInput />
+  <div class="min-h-screen px-4 py-12 sm:px-6">
+    <div>
+      <h1
+        class="flex-1 text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl"
+      >
+        Vue Filament UI
+      </h1>
+      <div class="flex flex-col items-start gap-6 pt-8">
+        <div>
+          <h2 class="text-lg font-medium text-gray-900">Buttons</h2>
+          <div class="flex flex-col gap-3 pt-2">
+            <Button>This is a button</Button>
+          </div>
+        </div>
+        <div>
+          <h2 class="text-lg font-medium text-gray-900">Text Input</h2>
+          <div class="flex flex-col gap-3 pt-2">
+            <TextInput />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
